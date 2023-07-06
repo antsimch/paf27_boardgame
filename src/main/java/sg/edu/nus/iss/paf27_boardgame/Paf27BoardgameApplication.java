a1185b8088be41c447a4f287c983f2de68fd600a
antsimch <anthonysimch@gmail.com>
2023-07-06 13:19:56 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/paf27_boardgame/Paf27BoardgameApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf27_boardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paf27BoardgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Paf27BoardgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/paf27_boardgame/Paf27BoardgameApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf27_boardgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Paf27BoardgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
